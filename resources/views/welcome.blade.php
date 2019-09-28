14bf9d583e420a2b73ddbbb265142775653ce726
Isco Amador <isco.amaador@gmail.com>
2019-09-28 12:18:35 -0500

    Cambiar titulo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    API Control Escolar
                 </div>
 
                 <div class="links">
