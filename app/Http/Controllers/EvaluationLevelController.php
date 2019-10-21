9770c9bf58a56c781c6e867b1fe37d019e7d60ba
Gema <gema.moran@cceo.com.mx>
2019-10-21 14:18:08 -0500

    Se añadieron las migraciones, modelos y controladores de las tablas sin llaves foráneas

---
+++ app/Http/Controllers/EvaluationLevelController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\EvaluationLevel;
+use Illuminate\Http\Request;
+
+class EvaluationLevelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\EvaluationLevel  $evaluationLevel
+     * @return \Illuminate\Http\Response
+     */
+    public function show(EvaluationLevel $evaluationLevel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\EvaluationLevel  $evaluationLevel
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(EvaluationLevel $evaluationLevel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\EvaluationLevel  $evaluationLevel
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, EvaluationLevel $evaluationLevel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\EvaluationLevel  $evaluationLevel
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(EvaluationLevel $evaluationLevel)
+    {
+        //
+    }
+}
