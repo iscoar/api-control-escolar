98fbde392cb25cedb9f931ea2c89660f0060681f
Gema <gema.moran@cceo.com.mx>
2019-11-04 00:31:44 -0600

    Se añadieron datos a GradesTableSeeder

--- database/migrations/2019_10_21_185844_create_grades_table.php
+++ database/migrations/2019_10_21_185844_create_grades_table.php
@@ -15,7 +15,7 @@ class CreateGradesTable extends Migration
     {
         Schema::create('grades', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name', 30)->nullable();
+            $table->string('name', 10);
             $table->timestamps();
         });
     }
--- database/seeds/GradesTableSeeder.php
+++ database/seeds/GradesTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class GradesTableSeeder extends Seeder
 {
@@ -11,6 +12,35 @@ class GradesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('grades')->insert([
+            'id' => 1,
+            'name' => '1RO',
+            'created_at' => now()
+        ]);
+        DB::table('grades')->insert([
+            'id' => 2,
+            'name' => '2DO',
+            'created_at' => now()
+        ]);
+        DB::table('grades')->insert([
+            'id' => 3,
+            'name' => '3RO',
+            'created_at' => now()
+        ]);
+        DB::table('grades')->insert([
+            'id' => 4,
+            'name' => '4TO',
+            'created_at' => now()
+        ]);
+        DB::table('grades')->insert([
+            'id' => 5,
+            'name' => '5TO',
+            'created_at' => now()
+        ]);
+        DB::table('grades')->insert([
+            'id' => 6,
+            'name' => '6TO',
+            'created_at' => now()
+        ]);
     }
 }
