b05928119fc0db588407aca01d672251bf8a87d4
Gema <gema.moran@cceo.com.mx>
2019-11-12 14:10:41 -0600

    Se añadieron los datos a StudentsGroupsTableSeeder

--- database/seeds/GroupsTableSeeder.php
+++ database/seeds/GroupsTableSeeder.php
@@ -13,37 +13,37 @@ class GroupsTableSeeder extends Seeder
     {
         DB::table('groups')->insert([
             'id' => 1,
-            'description' => 'ITI7B',
-            'students_quantity' => 20,
             'classroom_id' => 1,
             'grade_id' => 1,
+            'description' => 'ITI7B',
+            'students_quantity' => 20,
             'status' => 'INACTIVO',
             'created_at' => now()
         ]);
         DB::table('groups')->insert([
             'id' => 2,
-            'description' => 'ITI8B',
-            'students_quantity' => 20,
             'classroom_id' => 1,
             'grade_id' => 2,
+            'description' => 'ITI8B',
+            'students_quantity' => 20,
             'status' => 'INACTIVO',
             'created_at' => now()
         ]);
         DB::table('groups')->insert([
             'id' => 3,
-            'description' => 'ITI9B',
-            'students_quantity' => 20,
             'classroom_id' => 1,
             'grade_id' => 3,
+            'description' => 'ITI9B',
+            'students_quantity' => 20,
             'status' => 'INACTIVO',
             'created_at' => now()
         ]);
         DB::table('groups')->insert([
             'id' => 4,
-            'description' => 'ITI10B',
-            'students_quantity' => 20,
             'classroom_id' => 1,
             'grade_id' => 4,
+            'description' => 'ITI10B',
+            'students_quantity' => 20,
             'status' => 'ACTIVO',
             'created_at' => now()
         ]);
--- database/seeds/StudentGroupsTableSeeder.php
+++ database/seeds/StudentGroupsTableSeeder.php
@@ -11,6 +11,112 @@ class StudentGroupsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        //GRUPO CUATRIMESTRE 1
+        DB::table('student_groups')->insert([
+            'group_id' => 1,
+            'student_id' => 'R161306021',
+            'created_at' => now()
+        ]);
+        DB::table('student_groups')->insert([
+            'group_id' => 1,
+            'student_id' => 'R161306068',
+            'created_at' => now()
+        ]);
+        DB::table('student_groups')->insert([
+            'group_id' => 1,
+            'student_id' => 'R160306165',
+            'created_at' => now()
+        ]);
+        DB::table('student_groups')->insert([
+            'group_id' => 1,
+            'student_id' => 'R162506099',
+            'created_at' => now()
+        ]);
+        DB::table('student_groups')->insert([
+            'group_id' => 1,
+            'student_id' => 'R161305933',
+            'created_at' => now()
+        ]);
+
+         //GRUPO CUATRIMESTRE 2
+         DB::table('student_groups')->insert([
+            'group_id' => 2,
+            'student_id' => 'R161306021',
+            'created_at' => now()
+        ]);
+        DB::table('student_groups')->insert([
+            'group_id' => 2,
+            'student_id' => 'R161306068',
+            'created_at' => now()
+        ]);
+        DB::table('student_groups')->insert([
+            'group_id' => 2,
+            'student_id' => 'R160306165',
+            'created_at' => now()
+        ]);
+        DB::table('student_groups')->insert([
+            'group_id' => 2,
+            'student_id' => 'R162506099',
+            'created_at' => now()
+        ]);
+        DB::table('student_groups')->insert([
+            'group_id' => 2,
+            'student_id' => 'R161305933',
+            'created_at' => now()
+        ]);
+
+        //GRUPO CUATRIMESTRE 3
+        DB::table('student_groups')->insert([
+        'group_id' => 3,
+        'student_id' => 'R161306021',
+        'created_at' => now()
+        ]);
+        DB::table('student_groups')->insert([
+            'group_id' => 3,
+            'student_id' => 'R161306068',
+            'created_at' => now()
+        ]);
+        DB::table('student_groups')->insert([
+            'group_id' => 3,
+            'student_id' => 'R160306165',
+            'created_at' => now()
+        ]);
+        DB::table('student_groups')->insert([
+            'group_id' => 3,
+            'student_id' => 'R162506099',
+            'created_at' => now()
+        ]);
+        DB::table('student_groups')->insert([
+            'group_id' => 3,
+            'student_id' => 'R161305933',
+            'created_at' => now()
+        ]);
+
+        //GRUPO CUATRIMESTRE 4
+        DB::table('student_groups')->insert([
+            'group_id' => 4,
+            'student_id' => 'R161306021',
+            'created_at' => now()
+        ]);
+        DB::table('student_groups')->insert([
+            'group_id' => 4,
+            'student_id' => 'R161306068',
+            'created_at' => now()
+        ]);
+        DB::table('student_groups')->insert([
+            'group_id' => 4,
+            'student_id' => 'R160306165',
+            'created_at' => now()
+        ]);
+        DB::table('student_groups')->insert([
+            'group_id' => 4,
+            'student_id' => 'R162506099',
+            'created_at' => now()
+        ]);
+        DB::table('student_groups')->insert([
+            'group_id' => 4,
+            'student_id' => 'R161305933',
+            'created_at' => now()
+        ]);
     }
 }
